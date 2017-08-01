405a6f28c0277d1dd26e64242b0391afb862784b
Kaganovich Maksim <eliethewoody@y.ru>
2017-08-01 22:13:36 +0300

    rewrited this disgusting code a bit

--- src/compile.sh
+++ src/compile.sh
@@ -13,8 +13,12 @@ mkdir ./bin
 cd ./bin 
 cp ../*.ml ./
 cp ../*.mli ./
-ocamlopt rencralga.mli
-ocamlopt -o project.dev unix.cmxa str.cmxa rencralga.ml core.ml
+ocamlc -c rencralga.mli
+ocamlc -c rencralga.ml
+ocamlc -c io.mli
+ocamlc -c io.ml
+ocamlc -c core.ml
+ocamlc -o project.dev unix.cma str.cma rencralga.cmo io.cmo core.cmo
 rm ../*.dev
 cp ./*.dev ../
 echo "done! executable file: project.dev"

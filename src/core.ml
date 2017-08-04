e089d9bef7606d21d816079c98e0b4883605df5e
Kaganovich Maksim <eliethewoody@y.ru>
2017-08-04 21:37:25 +0300

    added key encryption support

--- src/core.ml
+++ src/core.ml
@@ -1,11 +1,12 @@
 open Rencralga;;
 open Io;;
+let key = [1;1;1;0;1;1;0;0;] in 
 let source = explode (read_full_file "./a.txt") in 
 let headers = generate_headers (List.length source) in 
 let safe = encrypt_text source headers in 
 record_message safe "./safe.q";
-record_key headers "./key.q" [1;1;0;0;];
-let r_headers = read_encrypted_key "./key.q" [1;1;0;0;]; in
+record_key headers "./key.q" key;
+let r_headers = read_encrypted_key "./key.q" key; in
 let r_safe = read_encrypted "./safe.q"; in 
 print r_headers;
 print_endline "\n--------------------------";

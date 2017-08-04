55fb4851e0b7a0ee2cddef19e8f4c410d9580a6c
Kaganovich Maksim <eliethewoody@y.ru>
2017-08-04 21:35:33 +0300

    rewrited a startup script

--- src/compile.sh
+++ src/compile.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-echo "we are here"
+echo "we are here:"
 PWD="`pwd`"
 echo "${PWD}"
 echo "copying source files to bin..."
 cd ..
-echo "we are here ${PWD}"
+echo "we are here: ${PWD}"
 ls -la
 rm -rf ./bin 
 mkdir ./bin 
 cp ./src/*.ml ./bin
 cp ./src/*.mli ./bin
 cd ./bin 
-echo "we are here ${PWD}"
+echo "we are here: ${PWD}"
 ls -la
 echo "done!"
 echo "starting compilation..."
-echo "we are here ${PWD}"
+echo "we are here: ${PWD}"
 ocamlc -c rencralga.mli
 ocamlc -c rencralga.ml
 ocamlc -c io.mli
@@ -24,12 +24,12 @@ ocamlc -c core.ml
 ocamlc -o project.dev unix.cma str.cma rencralga.cmo io.cmo core.cmo
 cp ./*.dev ../
 cd ..
-echo "we are here ${PWD}"
+echo "we are here: ${PWD}"
 chmod a+x ./*.dev
 echo "done!"
 echo "removing source files from bin..."
 cd ./bin
-echo "we are here ${PWD}"
+echo "we are here: ${PWD}"
 ls -la
 rm ./*.ml
 rm ./*.mli
@@ -37,7 +37,7 @@ rm ./*.cmi
 rm ./*.cmo
 ls -la
 cd ..
-echo "we are here ${PWD}"
+echo "we are here: ${PWD}"
 echo "done!"
 ./project.dev
 echo ""
